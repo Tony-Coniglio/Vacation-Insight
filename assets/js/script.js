48f5f0ea5f7668d790fed6335d3037cdf7293381
Allen Wang <ayw10864@gmail.com>
2022-10-10 15:58:35 -0400

    added functionality to city details/climate tab on the right

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,24 @@
-/* example api call for the hotels api, ignore during merge.
-const options = {
-	method: 'POST',
-	headers: {
-		'content-type': 'application/json',
-		'X-RapidAPI-Key': 'e82b41c8b4mshdce26feef0f8eeap1bc373jsn76d5e61b4aa4',
-		'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
-	},
-	body: '{"currency":"USD","eapid":1,"locale":"en_US","siteId":300000001,"destination":{"regionId":"6054439"},"checkInDate":{"day":10,"month":10,"year":2022},"checkOutDate":{"day":15,"month":10,"year":2022},"rooms":[{"adults":2,"children":[{"age":5},{"age":7}]}],"resultsStartingIndex":0,"resultsSize":200,"sort":"PRICE_LOW_TO_HIGH","filters":{"price":{"max":150,"min":100}}}'
-};
+var citiesTab = $('#city-tab');
 
-fetch('https://hotels4.p.rapidapi.com/properties/v2/list', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-*/
+citiesTab.on('click', citiesTabClicked);
+
+
+//swaps the city detail tab based on which one is focused.
+function citiesTabClicked (event)
+{
+	var parent = $(event.target).parent();
+	var sibling = $(parent).siblings();
+	if(!parent.hasClass('is-active'))
+	{
+		var currentActiveDiv = $('div.is-visible');
+		var currentInactiveDiv = $('div.is-hidden');
+		sibling.removeClass('is-active');
+		parent.addClass('is-active');
+		currentActiveDiv.removeClass('is-visible');
+		currentActiveDiv.addClass('is-hidden');
+		currentInactiveDiv.removeClass('is-hidden');
+		currentInactiveDiv.addClass('is-visible');
+	}
+	console.log(sibling.hasClass('is-active'));
+	console.log(parent.hasClass('is-active'));
+}
